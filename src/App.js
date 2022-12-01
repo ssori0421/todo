bb19137f1eb1abbaf7b3cf5fabf792b48fe2fb4f
ssori0421 <ssori0421@gmail.com>
2022-12-01 11:19:46 +0900

    todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+const TODO_LIST = [
+  {
+    id: 1,
+    content: "react를 배워봅시다",
+  },
+  {
+    id: 2,
+    content: "useState를 배워봅시다",
+  },
+  {
+    id: 3,
+    content: "자바스크립트를 배워봅시다",
+  },
+];
 
 function App() {
+  const [list, setList] = useState(TODO_LIST);
+  const [content, setContent] = useState("");
+  const onChangeHandler = (e) => {
+    setContent(e.target.value);
+  };
+  const onClick = () => {
+    setList([...list, { id: list.length + 1, content }]);
+    setContent("");
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="text" value={content} onChange={onChangeHandler} />
+        <button onClick={onClick}>추가하기</button>
       </header>
+      <div className="App-title">
+        <h1>Todo List</h1>  
+      </div>
+      <div className="cardWrap">
+        {list.map((item) => (
+          <div className="card" key={item.id}>
+            {item.content}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
